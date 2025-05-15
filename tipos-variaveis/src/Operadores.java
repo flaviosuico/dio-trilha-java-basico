2ced0741eb4f1265fc842c47b41e25d00ee5c4a8
flaviosuico <flaviosuico@gmail.com>
2025-05-15 19:21:45 -0300

    remove exemplos de comparação entre strings e números, mantendo lógica booleana

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,27 +1,14 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeUmString = "Lucas";
-        String nomeDoisString = "Lucas2";
-
-        System.out.println("nomeUm ");
-
-
-        int numero1 = 1;
-        int numero2 = 2;
-        boolean simNao = numero1 == numero2;
-        System.out.println("numero1 é igual a numero2? " + simNao);
-
-        simNao = numero1 != numero2;
-        System.out.println("numero1 é diferente de numero2? " + simNao);
-        
-        if (numero1 < numero2) {
-            System.out.println("numero1 é menor que numero2");
+        boolean a = true;
+        boolean b = false;
+        if(a && b) {
+            System.out.println("Ambos son verdaderos");
+        } else if (a || b) {
+            System.out.println("Al menos uno es verdadero");
+        } else {
+            System.out.println("Ninguno es verdadero");
         }
-        else {
-            System.out.println("numero1 é maior que numero2");
-        }
-
-        System.out.println(nomeUmString.equals(nomeDoisString));
         
     }
 }
