498cab35b946688ef0a769624b14e72f655a3193
flaviosuico <flaviosuico@gmail.com>
2025-05-15 18:34:20 -0300

    commit 2

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.00;
+        
+        short numeroCurto = 1;
+        int numeroNormal = 10000;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
+
     }
 }
