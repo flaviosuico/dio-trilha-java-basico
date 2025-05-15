37274e35ea71059697b963259c88137eb323d908
flaviosuico <flaviosuico@gmail.com>
2025-05-15 18:59:41 -0300

    remove exemplo de negação booleana e adiciona comparação entre variáveis

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,13 @@
 public class Operadores {
     public static void main(String[] args) {
         
-        // exemplo de bolleana negando 
-        boolean variavel = true;
-        System.out.println(!variavel); // false
+        int a, b;
+        a = 5;
+        b = 5;
+        String resultado = (a == b) ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
+        
         
         
     }
